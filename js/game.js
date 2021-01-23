d73d0c7bec8f25ab539e8ce8e27f0100c2a3db26
Jaime <jaimeferncast@gmail.com>
2021-01-23 12:12:57 +0100

    mouse events addded v2

--- js/game.js
+++ js/game.js
@@ -24,71 +24,70 @@ const appGame = {
         this.drawAll()
     },
     setEventListeners() {
-        // let isDown = false
-        // let slingCenterX = this.slingShot.position.x + this.slingShot.size.w / 2
-        // let slingCenterY = this.slingShot.position.y + this.slingShot.size.h / 2
-        // this.canvasDOM.addEventListener('mousedown', e => {
-        //     this.slingShot.position.x + this.slingShot.size.w > e.offsetX && e.offsetX > this.slingShot.position.x && this.slingShot.position.y + this.slingShot.size.h > e.offsetY && e.offsetY > this.slingShot.position.y ? isDown = true : null
-        //     this.ctx.beginPath();
-        //     this.ctx.strokeStyle = 'black';
-        //     this.ctx.lineWidth = 1;
-        //     this.ctx.moveTo(slingCenterX, slingCenterY);
-        //     this.ctx.lineTo(e.offsetX, e.offsetY);
-        //     this.ctx.stroke();
-        //     this.ctx.closePath()
-        // })
-        // this.canvasDOM.addEventListener('mousemove', e => {
-        //     if (isDown) {
-        //         this.ctx.beginPath();
-        //         this.ctx.strokeStyle = 'black';
-        //         this.ctx.lineWidth = 1;
-        //         this.ctx.moveTo(slingCenterX, slingCenterY);
-        //         this.ctx.lineTo(e.offsetX, e.offsetY);
-        //         this.ctx.stroke();
-        //         this.ctx.closePath()
-        //     }
-        // })
-        // this.canvasDOM.addEventListener('mouseup', e => {
-        //     isDown = false
-        // })
-        this.canvasDOM.addEventListener('drag', e => {
+        let isDown = false
+        let slingCenter = {
+            x: this.slingShot.position.x + this.slingShot.size.w / 2,
+            y: this.slingShot.position.y + this.slingShot.size.h / 2
+        }
+        let pointerPosition = { x: undefined, y: undefined }
+
+        this.canvasDOM.addEventListener('mousedown', e => {
             this.slingShot.position.x + this.slingShot.size.w > e.offsetX && e.offsetX > this.slingShot.position.x && this.slingShot.position.y + this.slingShot.size.h > e.offsetY && e.offsetY > this.slingShot.position.y ? isDown = true : null
-        },
-            setDimensions() {
-            this.canvasSize = {
-                w: 900,
-                h: window.innerHeight
+        })
+
+        this.canvasDOM.addEventListener('mousemove', e => {
+            pointerPosition.x = e.offsetX
+            pointerPosition.y = e.offsetY
+            if (isDown && pointerPosition.y > slingCenter.y) {
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = 'black'
+                this.ctx.lineWidth = 1
+                this.ctx.moveTo(slingCenter.x, slingCenter.y)
+                this.ctx.lineTo(pointerPosition.x, pointerPosition.y)
+                this.ctx.stroke()
+                this.ctx.closePath()
             }
+        })
+
+        this.canvasDOM.addEventListener('mouseup', e => {
+            isDown = false
+        })
+    },
+    setDimensions() {
+        this.canvasSize = {
+            w: 900,
+            h: window.innerHeight
+        }
         this.canvasDOM.setAttribute('width', this.canvasSize.w)
         this.canvasDOM.setAttribute('height', this.canvasSize.h)
-        },
-            drawAll() {
-                setInterval(() => {
-    this.clearScreen()
-    this.frames++
-    this.slingShot.draw()
-    this.enemies.forEach(elm => {
-        elm.draw()
-    })
-    this.frames % 50 === 0 ? this.createEnemies() : null
-    this.frames % 50 === 0 ? this.clearEnemy() : null
-}, 70);
     },
-clearScreen() {
-    this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
-},
-createSlingshot() {
-    this.slingShot = new Slingshot(this.ctx, this.canvasSize, this.canvasSize.w / 2 - 25, this.canvasSize.h - 100, 50, 50)
-},
-createEnemies() {
-    this.enemies.push(new Enemy(this.ctx, this.canvasSize))
-},
-createBombs() {
+    drawAll() {
+        setInterval(() => {
+            this.clearScreen()
+            this.frames++
+            this.slingShot.draw()
+            this.enemies.forEach(elm => {
+                elm.draw()
+            })
+            this.frames % 50 === 0 ? this.createEnemies() : null
+            this.frames % 50 === 0 ? this.clearEnemy() : null
+        }, 70)
+    },
+    clearScreen() {
+        this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
+    },
+    createSlingshot() {
+        this.slingShot = new Slingshot(this.ctx, this.canvasSize, this.canvasSize.w / 2 - 25, this.canvasSize.h - 100, 50, 50)
+    },
+    createEnemies() {
+        this.enemies.push(new Enemy(this.ctx, this.canvasSize))
+    },
+    createBombs() {
 
-},
-clearEnemy() {
-    this.enemies.forEach((elm, i) => {
-        elm.position.y > this.canvasSize.h ? this.enemies.splice(i, 1) : null
-    })
-}
+    },
+    clearEnemy() {
+        this.enemies.forEach((elm, i) => {
+            elm.position.y > this.canvasSize.h ? this.enemies.splice(i, 1) : null
+        })
+    }
 }
